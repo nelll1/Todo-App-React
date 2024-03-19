5abcb03a3ec9f19cca6715f07c3eb8bf174f12ed
nelll1 <92243663+nelll1@users.noreply.github.com>
2024-03-19 14:15:07 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import './Style/Todo.css'
+function App() {
+
+    let [Todo, setTodo] = useState("")
+    let [toDoList, setToDoList] = useState([])
+
+
+    const handleChange = (event) =>{
+      const newValue = event.target.value;
+      setTodo(newValue)
+    }
+
+    const handleSubmit = () => {
+      setToDoList((prevValues) => {
+        return [...prevValues, ...Todo]
+      })
+    }
+
+    const getList = toDoList.map(toDoItem => <li>{toDoItem}</li>)
+
+    
+ 
+  return ( 
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" onChange={handleChange} value={Todo}/>
+        <button onClick={handleSubmit}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {getList}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
